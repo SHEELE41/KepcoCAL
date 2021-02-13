f8a368678aad7512c7aff239bf506831fc0179dc
sheele41 <bbang0_0@daum.net>
2021-02-13 19:09:47 +0900

    ReportCellFormListViewModel.kt Remove Redundant Methods

--- app/src/main/java/com/mevius/kepcocal/ui/report_cell_form_list/ReportCellFormListViewModel.kt
+++ app/src/main/java/com/mevius/kepcocal/ui/report_cell_form_list/ReportCellFormListViewModel.kt
@@ -12,14 +12,8 @@ import kotlinx.coroutines.launch
 class ReportCellFormListViewModel @ViewModelInject constructor(
     private val cellFormRepository: CellFormRepository
 ): ViewModel() {
-    val allCellForms: LiveData<List<CellForm>> = cellFormRepository.allCellForms
-
     fun getCellFormsWithReportId(reportId: Long): LiveData<List<CellForm>>{
         return cellFormRepository.getCellFormsWithReportId(reportId)
-    }
-
-    fun insertCellForm(cellForm: CellForm) = viewModelScope.launch(Dispatchers.IO) {
-        cellFormRepository.insert(cellForm)
     }
 
     fun deleteCellForm(cellForm: CellForm) = viewModelScope.launch(Dispatchers.IO) {
